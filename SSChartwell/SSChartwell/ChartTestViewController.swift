5601a78f23bfc6c405f2b129b6b7c35932941261
Jeffrey Bergier <jeffburg@jeffburg.com>
2015-11-11 23:17:03 -0800

    writing special data types for the rings. Need to figure out how to do sequence type.

--- SSChartwell/SSChartwell/ChartTestViewController.swift
+++ SSChartwell/SSChartwell/ChartTestViewController.swift
@@ -16,95 +16,19 @@ class ChartTestViewController: NSViewController {
         super.viewDidLoad()
         
         var data = ChartwellRingData()
-        data.ring1 = 10
-        
-        let renderer = ChartwellRingRenderer(data: data, pointSize: 20)
-        let tiff = renderer.TIFFRepresentation
+        data.ring1 = ChartwellRingValue(size: .Ten, color: NSColor.blueColor())
+        data.ring2 = ChartwellRingValue(size: .Twenty, color: NSColor.redColor())
+        data.ring3 = ChartwellRingValue(size: .Thirty, color: NSColor.yellowColor())
+        data.ring4 = ChartwellRingValue(size: .Forty, color: NSColor.greenColor())
+        data.ring5 = ChartwellRingValue(size: .Fifty, color: NSColor.blackColor())
+        data.ring6 = ChartwellRingValue(size: .Sixty, color: NSColor.grayColor())
+        data.ring7 = ChartwellRingValue(size: .Seventy, color: NSColor.darkGrayColor())
+        data.ring8 = ChartwellRingValue(size: .Eighty, color: NSColor.lightGrayColor())
+        data.ring9 = ChartwellRingValue(size: .Ninety, color: NSColor.cyanColor())
+        data.ring10 = ChartwellRingValue(size: .OneHundred, color: NSColor.magentaColor())
+
+        let renderer = ChartwellRingRenderer(data: data, pointSize: 100)
+        let tiff = renderer.TIFFImage
         print(tiff)
     }
-    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        let textArray = [10,30,50,70,40,80,90,100,10,20,30,40,50,60,70,80,90,100,10,30,50,70,40,80,90,100,10,20,30,40,50,60,70,80,90,100,10,30,50,70,40,80,90,100,10,20,30,40,50,60,70,80,90,100]
-//        
-//        let textTuple = [
-//            (value: 10, color: NSColor.redColor()),
-//            (value: 20, color: NSColor.yellowColor()),
-//            (value: 30, color: NSColor.greenColor()),
-//            (value: 40, color: NSColor.blueColor()),
-//            (value: 50, color: NSColor.grayColor()),
-//            (value: 60, color: NSColor.darkGrayColor()),
-//            (value: 70, color: NSColor.lightGrayColor()),
-//            (value: 80, color: NSColor.blueColor()),
-//            (value: 90, color: NSColor.brownColor()),
-//            (value: 100, color: NSColor.magentaColor()),
-//            (value: 10, color: NSColor.cyanColor()),
-//            (value: 20, color: NSColor.orangeColor()),
-//            (value: 30, color: NSColor.redColor()),
-//            (value: 40, color: NSColor.redColor()),
-//            (value: 50, color: NSColor.greenColor()),
-//            (value: 60, color: NSColor.grayColor()),
-//            (value: 70, color: NSColor.yellowColor()),
-//            (value: 80, color: NSColor.darkGrayColor()),
-//            (value: 90, color: NSColor.orangeColor()),
-//            (value: 100, color: NSColor.redColor()),
-//            (value: 10, color: NSColor.blueColor()),
-//            (value: 20, color: NSColor.greenColor()),
-//            (value: 30, color: NSColor.magentaColor()),
-//            (value: 40, color: NSColor.cyanColor()),
-//            (value: 50, color: NSColor.blueColor()),
-//            (value: 60, color: NSColor.redColor()),
-//            (value: 70, color: NSColor.blueColor()),
-//            (value: 80, color: NSColor.greenColor()),
-//            (value: 90, color: NSColor.magentaColor()),
-//            (value: 100, color: NSColor.cyanColor())
-//        ]
-//        
-//        let rendererArray = [
-//            ChartWellFontRenderer(style: .Bars, pointSize: 100),
-//            ChartWellFontRenderer(style: .BarsVertical, pointSize: 100),
-//            ChartWellFontRenderer(style: .Lines, pointSize: 100),
-//            ChartWellFontRenderer(style: .Pies, pointSize: 100),
-//            ChartWellFontRenderer(style: .Radar, pointSize: 100),
-//            ChartWellFontRenderer(style: .Rings, pointSize: 200),
-//            ChartWellFontRenderer(style: .Rose, pointSize: 100)
-//        ]
-//        
-//        let chartImageArray = rendererArray.map() { font -> PDFData in
-//            return font.chartImageFromIntegerArray(textArray, chartColor: NSColor.blueColor())
-//        }
-//        
-//        let chartColoredArray = rendererArray.map() { font -> PDFData in
-//            return font.chartImageFromTuple(textTuple)
-//        }
-//        
-//        // MARK: For some reason native views only show in the stackview if there are also imageviews
-//        
-//        for (_, data) in chartColoredArray.enumerate() {
-//            let imageView = NSImageView()
-//            imageView.image = NSImage(data: data)
-//            self.imageStackView?.addView(imageView, inGravity: .Bottom)
-//        }
-//        
-//        for (_, data) in chartImageArray.enumerate() {
-//            let imageView = NSImageView()
-//            imageView.image = NSImage(data: data)
-//            self.imageStackView?.addView(imageView, inGravity: .Bottom)
-//        }
-//        
-//        // MARK: For Future
-//        
-//        for colorRenderer in rendererArray {
-//            let newView = colorRenderer.chartViewFromTuple(textTuple)
-//            self.imageStackView?.addView(newView, inGravity: .Bottom)
-//        }
-//        
-//        for bwRenderer in rendererArray {
-//            let newView = bwRenderer.chartViewFromIntegerArray(textArray, chartColor: NSColor.redColor())
-//            self.imageStackView?.addView(newView, inGravity: .Bottom)
-//        }
-//        
-//    }
-    
 }
--- SSChartwell/SSChartwell/ChartWellFont.swift
+++ SSChartwell/SSChartwell/ChartWellFont.swift
@@ -10,17 +10,83 @@ import Cocoa
 
 typealias PDFData = NSData
 
-struct ChartwellRingData {
-    var ring1: UInt = 0
-    var ring2: UInt = 0
-    var ring3: UInt = 0
-    var ring4: UInt = 0
-    var ring5: UInt = 0
-    var ring6: UInt = 0
-    var ring7: UInt = 0
-    var ring8: UInt = 0
-    var ring9: UInt = 0
-    var ring10: UInt = 0
+enum ZeroOneHundredInt: Int, CustomStringConvertible {
+    case Zero = 0, One, Two, Three, Four, Five, Size, Seven, Eight, Nine, Ten, Eleven, Twelve, Thirteen, Fourteen, Fifteen, Sixteen, Seventeen, Eighteen, Nineteen, Twenty, TwentyOne, TwentyTwo, TwentyThree, TwentyFour, TwentyFive, TwentySix, TwentySeven, TwentyEight, TwentyNine, Thirty, ThirtyOne, ThirtyTwo, ThirtyThree, ThirtyFour, ThirtyFive, ThirtySix, ThirtySeven, ThirtyEight, ThirtyNine, Forty, FortyOne, FortyTwo, FortyThree, Fortyfour, FortyFive, FortySix, FortySeven, FortyEight, FortyNine, Fifty, FiftyOne, FiftyTwo, FiftyThree, FiftyFour, FiftyFive, FiftySix, FiftySeven, FiftyEight, FiftyNine, Sixty, SixtyOne, SixtyTwo, SixtyThree, SixtyFour, SixtyFive, SixtySix, SixtySeven, SixtyEight, SixtyNine, Seventy, SeventyOne, SeventyTwo, SeventyThree, SeventyFour, SeventyFive, SeventySix, SeventySeven, SeventyEight, SeventyNine, Eighty, EightyOne, EightyTwo, EightyThree, EightyFour, EightyFive, EightySix, EightySeven, EightyEight, EightyNine, Ninety, NinetyOne, NinetyTwo, NinetyThree, NinetyFour, NinetyFive, NinetySix, NinetySeven, NinetyEight, NinetyNine, OneHundred
+    
+    var description: String {
+        return "\(self.rawValue)"
+    }
+}
+
+struct ChartwellRingValue {
+    var size: ZeroOneHundredInt = .Zero
+    var color: NSColor = NSColor.clearColor()
+}
+
+struct ChartwellRingData: GeneratorType, SequenceType {
+    var ring1 = ChartwellRingValue()
+    var ring2 = ChartwellRingValue()
+    var ring3 = ChartwellRingValue()
+    var ring4 = ChartwellRingValue()
+    var ring5 = ChartwellRingValue()
+    var ring6 = ChartwellRingValue()
+    var ring7 = ChartwellRingValue()
+    var ring8 = ChartwellRingValue()
+    var ring9 = ChartwellRingValue()
+    var ring10 = ChartwellRingValue()
+    
+    private var iteratingLocation = 0
+    
+    mutating func next() -> ChartwellRingValue? {
+        self.iteratingLocation += 1
+        switch self.iteratingLocation {
+        case 0:
+            return self.ring1
+        case 1:
+            return self.ring2
+        case 2:
+            return self.ring3
+        case 3:
+            return self.ring4
+        case 4:
+            return self.ring5
+        case 5:
+            return self.ring6
+        case 6:
+            return self.ring7
+        case 7:
+            return self.ring8
+        case 8:
+            return self.ring9
+        case 9:
+            return self.ring10
+        default:
+            self.iteratingLocation = 0
+            return .None
+        }
+    }
+}
+
+extension NSAttributedString {
+    
+    convenience init(ringData: ChartwellRingData, font: NSFont) {
+        let mutableAttributedString = NSMutableAttributedString()
+        for ring in ringData {
+            
+        }
+        mutableAttributedString.appendAttributedString(NSAttributedString(string: "\(ringData.ring1.size)+", attributes: [NSForegroundColorAttributeName : ringData.ring1.color, NSFontAttributeName : font]))
+        mutableAttributedString.appendAttributedString(NSAttributedString(string: "\(ringData.ring2.size)+", attributes: [NSForegroundColorAttributeName : ringData.ring2.color, NSFontAttributeName : font]))
+        mutableAttributedString.appendAttributedString(NSAttributedString(string: "\(ringData.ring3.size)+", attributes: [NSForegroundColorAttributeName : ringData.ring3.color, NSFontAttributeName : font]))
+        mutableAttributedString.appendAttributedString(NSAttributedString(string: "\(ringData.ring4.size)+", attributes: [NSForegroundColorAttributeName : ringData.ring4.color, NSFontAttributeName : font]))
+        mutableAttributedString.appendAttributedString(NSAttributedString(string: "\(ringData.ring5.size)+", attributes: [NSForegroundColorAttributeName : ringData.ring5.color, NSFontAttributeName : font]))
+        mutableAttributedString.appendAttributedString(NSAttributedString(string: "\(ringData.ring6.size)+", attributes: [NSForegroundColorAttributeName : ringData.ring6.color, NSFontAttributeName : font]))
+        mutableAttributedString.appendAttributedString(NSAttributedString(string: "\(ringData.ring7.size)+", attributes: [NSForegroundColorAttributeName : ringData.ring7.color, NSFontAttributeName : font]))
+        mutableAttributedString.appendAttributedString(NSAttributedString(string: "\(ringData.ring8.size)+", attributes: [NSForegroundColorAttributeName : ringData.ring8.color, NSFontAttributeName : font]))
+        mutableAttributedString.appendAttributedString(NSAttributedString(string: "\(ringData.ring9.size)+", attributes: [NSForegroundColorAttributeName : ringData.ring9.color, NSFontAttributeName : font]))
+        mutableAttributedString.appendAttributedString(NSAttributedString(string: "\(ringData.ring10.size)", attributes: [NSForegroundColorAttributeName : ringData.ring10.color, NSFontAttributeName : font]))
+        self.init(attributedString: mutableAttributedString)
+        
+    }
 }
 
 struct ChartwellRingRenderer {
@@ -32,19 +98,22 @@ struct ChartwellRingRenderer {
         self.data = data
     }
     
-    var TIFFRepresentation: NSData {
+    var TIFFImage: NSImage {
+        let PDFImage = self.PDFImage
+        let TIFF = PDFImage.TIFFRepresentation!
+        let TIFFImage = NSImage(data: TIFF)!
+        return TIFFImage
+    }
+    
+    var PDFImage: NSImage {
         let renderingView = NSTextField()
-        renderingView.wantsLayer = true
-        renderingView.backgroundColor = NSColor.redColor()
         renderingView.lineBreakMode = NSLineBreakMode.ByClipping
-        renderingView.stringValue = "Hello"
+        renderingView.attributedStringValue = NSAttributedString(ringData: self.data, font: self.font)
         renderingView.editable = false
         renderingView.sizeToFit()
-        let bitmapRep = renderingView.bitmapImageRepForCachingDisplayInRect(renderingView.bounds)
-        let pdf = renderingView.dataWithPDFInsideRect(renderingView.bounds)
-        let pdfImage = NSImage(data: pdf)
-        let tiff = pdfImage!.TIFFRepresentation
-        return bitmapRep!.TIFFRepresentation!
+        let PDF = renderingView.dataWithPDFInsideRect(renderingView.bounds)
+        let PDFImage = NSImage(data: PDF)!
+        return PDFImage
     }
 }
 
@@ -74,90 +143,6 @@ enum ChartwellFontStyle {
         case .Rose:
             return "Chartwell Rose"
         }
-    }
-}
-
-struct ChartWellFontRenderer {
-    var font: NSFont
-    var style: ChartwellFontStyle
-    private let renderingTextField: NSTextField
-    
-    init(style: ChartwellFontStyle, pointSize: CGFloat) {
-        
-        let rawFont =  NSFont.chartwellFontOfStyle(style, pointSize: pointSize)
-        let rawFontDescriptor = CTFontCopyFontDescriptor(rawFont)
-        let mutableRawFontDescriptor = CTFontDescriptorCreateCopyWithFeature(rawFontDescriptor,
-            NSNumber(int: 35), // Number Case
-            NSNumber(int: 2)) // Lining Figures
-        let adjustedFont = CTFontCreateWithFontDescriptor(mutableRawFontDescriptor, pointSize, nil)
-        
-        self.renderingTextField = NSTextField()
-        self.renderingTextField.font = adjustedFont
-        self.renderingTextField.lineBreakMode = NSLineBreakMode.ByClipping
-        
-        self.font = adjustedFont
-        self.style = style
-    }
-    
-    func chartImageFromTuple(array: [(value: Int, color: NSColor)]) -> PDFData {
-        let inputString = self.chartStringFromTupleArray(array)
-        self.renderingTextField.attributedStringValue = inputString
-        self.renderingTextField.sizeToFit()
-        let data = self.renderingTextField.dataWithPDFInsideRect(self.renderingTextField.bounds)
-        return data
-    }
-    
-    func chartViewFromTuple(array: [(value: Int, color: NSColor)]) -> NSView {
-        let inputString = self.chartStringFromTupleArray(array)
-        let renderingView = NSTextField()
-        renderingView.lineBreakMode = NSLineBreakMode.ByClipping
-        renderingView.editable = false
-        renderingView.attributedStringValue = inputString
-        renderingView.sizeToFit()
-        return renderingView
-    }
-    
-    func chartImageFromIntegerArray(chartInput: [Int], chartColor: NSColor) -> PDFData {
-        let inputString = self.chartStringFromIntegerArray(chartInput)
-        self.renderingTextField.stringValue = inputString
-        self.renderingTextField.textColor = chartColor
-        self.renderingTextField.sizeToFit()
-        let data = self.renderingTextField.dataWithPDFInsideRect(self.renderingTextField.bounds)
-        return data
-    }
-    
-    func chartViewFromIntegerArray(chartInput: [Int], chartColor: NSColor) -> NSView {
-        let inputString = self.chartStringFromIntegerArray(chartInput)
-        let renderingView = NSTextField()
-        renderingView.font = self.font
-        renderingView.lineBreakMode = NSLineBreakMode.ByClipping
-        renderingView.stringValue = inputString
-        renderingView.editable = false
-        renderingView.textColor = chartColor
-        renderingView.sizeToFit()
-        return renderingView
-    }
-    
-    private func chartStringFromIntegerArray(integerArray: [Int]) -> String {
-        var outputString = ""
-        integerArray.forEach() { integer in
-            outputString += "\(integer)+"
-        }
-        return outputString
-    }
-    
-    private func chartStringFromTupleArray(array: [(value: Int, color: NSColor)]) -> NSAttributedString {
-        let mutableAttributedString = NSMutableAttributedString()
-        array.forEach() { tuple in
-            let attributes = [
-                NSForegroundColorAttributeName : tuple.color,
-                NSFontAttributeName : self.font
-            ]
-            let tinyString = NSAttributedString(string: "\(tuple.value)+", attributes: attributes)
-            mutableAttributedString.appendAttributedString(tinyString)
-        }
-        let outputAttributedString = mutableAttributedString.copy() as! NSAttributedString
-        return outputAttributedString
     }
 }
 
