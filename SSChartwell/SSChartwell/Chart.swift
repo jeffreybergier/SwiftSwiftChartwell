c8971ef6f5cd5a2e0a255160e4b7970e51f5f29b
Jeffrey Bergier <jeffburg@jeffburg.com>
2015-11-19 18:49:55 -0800

    finished protocol extensions

--- SSChartwell/SSChartwell/Chart.swift
+++ SSChartwell/SSChartwell/Chart.swift
@@ -40,14 +40,149 @@ struct Chart {
     }
 }
 
+protocol ChartDataType {
+    static var max: UInt? { get }
+    static var fontName: String { get }
+    var components: [ChartDataComponentType] { get set }
+    init(components: [ChartDataComponentType])
+    mutating func appendComponent(newComponent: ChartDataComponentType) -> Bool
+    // WORKAROUND FOR SWIFT PROTOCOL EXTENSION
+    // ISSUES WITH INITIALIZERS
+    init()
+    // END WORKAROUND
+}
+
+protocol ChartDataComponentType {
+    static var max: UInt? { get }
+    var value: UInt { get set }
+    var color: NSColor { get set }
+    init(value: UInt, color: NSColor)
+    // WORKAROUND FOR SWIFT PROTOCOL EXTENSION
+    // ISSUES WITH INITIALIZERS
+    init()
+    // END WORKAROUND
+}
+
+protocol ChartRendererType {
+    var fontSize: CGFloat { get set }
+    var data: ChartDataType { get set }
+    var TIFFImage: NSImage { get }
+    var PDFImage: NSImage { get }
+    init(data: ChartDataType, fontSize: CGFloat)
+    // WORKAROUND FOR SWIFT PROTOCOL EXTENSION
+    // ISSUES WITH INITIALIZERS
+    init()
+    // END WORKAROUND
+}
+
+extension ChartDataType {
+    init(components: [ChartDataComponentType]) {
+        self.init()
+        if let max = self.dynamicType.max {
+            // If MAX has a value, then we need to make sure we don't exceed the max when appending
+            var componentsValue = UInt(0)
+            for component in components {
+                componentsValue += component.value
+                if componentsValue <= max {
+                    self.components.append(component)
+                } else {
+                    NSLog("Some components omitted because they exceeded the MAX value: \(max)")
+                    break
+                }
+            }
+        } else {
+            self.components = components
+        }
+    }
+    mutating func appendComponent(newComponent: ChartDataComponentType) -> Bool {
+        if let max = self.dynamicType.max {
+            // If MAX has a value, then we need to make sure we don't exceed the max when appending
+            var currentValue: UInt = 0
+            for component in self.components {
+                currentValue += component.value
+            }
+            if currentValue + newComponent.value <= max {
+                self.components.append(newComponent)
+                return true
+            } else {
+                NSLog("New component not appended because the existing components plus the new component exceeded the MAX value: \(max)")
+                return false
+            }
+        } else {
+            // If MAX is NIL then that means there is no max, so we can always append
+            self.components.append(newComponent)
+            return true
+        }
+    }
+}
+
+extension ChartDataComponentType {
+    init(value: UInt, color: NSColor) {
+        self.init()
+        self.color = color
+        self.value = value
+    }
+}
+
+extension ChartRendererType {
+    init(data: ChartDataType, fontSize: CGFloat) {
+        self.init()
+        self.data = data
+        self.fontSize = fontSize
+    }
+    
+    var TIFFImage: NSImage {
+        let PDFImage = self.PDFImage
+        let TIFF = PDFImage.TIFFRepresentation!
+        let TIFFImage = NSImage(data: TIFF)!
+        return TIFFImage
+    }
+    var PDFImage: NSImage {
+        let font = NSFont.chartwellFont(self.data, pointSize: self.fontSize)
+        let renderingView = NSTextField()
+        renderingView.lineBreakMode = NSLineBreakMode.ByClipping
+        renderingView.attributedStringValue = NSAttributedString(chartData: self.data, font: font)
+        renderingView.editable = false
+        renderingView.sizeToFit()
+        let PDF = renderingView.dataWithPDFInsideRect(renderingView.bounds)
+        let PDFImage = NSImage(data: PDF)!
+        return PDFImage
+    }
+}
+
+extension Chart {
+    struct Renderer: ChartRendererType {
+        var data: ChartDataType
+        var fontSize: CGFloat
+        
+        // WORKAROUND FOR SWIFT PROTOCOL EXTENSION
+        // ISSUES WITH INITIALIZERS
+        init() {
+            self.data = Chart.BarsVertical(components: [])
+            self.fontSize = 0
+        }
+        // END WORKAROUND
+    }
+}
+
 extension NSFont {
-    class func chartwellFontOfStyle(chartwell: Chart.Style, pointSize: CGFloat) -> NSFont {
-        let rawFont = NSFont(name: chartwell.fontName, size: pointSize)
+    class func chartwellFont(chartType: ChartDataType, pointSize: CGFloat) -> NSFont {
+        let rawFont = NSFont(name: chartType.dynamicType.fontName, size: pointSize)
         let rawFontDescriptor = CTFontCopyFontDescriptor(rawFont!)
         let mutableRawFontDescriptor = CTFontDescriptorCreateCopyWithFeature(rawFontDescriptor,
             NSNumber(int: 35), // Number Case
             NSNumber(int: 2)) // Lining Figures
         let adjustedFont = CTFontCreateWithFontDescriptor(mutableRawFontDescriptor, pointSize, nil)
         return adjustedFont
+    }
+}
+
+extension NSAttributedString {
+    convenience init(chartData: ChartDataType, font: NSFont) {
+        let mutableAttributedString = NSMutableAttributedString()
+        for component in chartData.components {
+            mutableAttributedString.appendAttributedString(NSAttributedString(string: "\(component.value)+", attributes: [NSForegroundColorAttributeName : component.color, NSFontAttributeName : font]))
+        }
+        self.init(attributedString: mutableAttributedString)
     }
 }
--- SSChartwell/SSChartwell/ChartTestViewController.swift
+++ SSChartwell/SSChartwell/ChartTestViewController.swift
@@ -15,59 +15,28 @@ class ChartTestViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let barComponents: [ChartDataComponentType]? = [
+        let barComponents: [ChartDataComponentType] = [
             Chart.BarsVertical.Component(value: 20, color: NSColor.redColor()),
-            Chart.BarsVertical.Component(value: 50, color: NSColor.blueColor())
+            Chart.BarsVertical.Component(value: 50, color: NSColor.blueColor()),
+            Chart.BarsVertical.Component(value: 120, color: NSColor.blueColor()),
+            Chart.BarsVertical.Component(value: 100, color: NSColor.redColor()),
+            Chart.BarsVertical.Component(value: 20, color: NSColor.yellowColor()),
+            Chart.BarsVertical.Component(value: 32, color: NSColor.greenColor()),
+            Chart.BarsVertical.Component(value: 75, color: NSColor.blackColor()),
+            Chart.BarsVertical.Component(value: 89, color: NSColor.grayColor()),
+            Chart.BarsVertical.Component(value: 45, color: NSColor.darkGrayColor()),
+            Chart.BarsVertical.Component(value: 22, color: NSColor.lightGrayColor()),
+            Chart.BarsVertical.Component(value: 12, color: NSColor.cyanColor()),
+            Chart.BarsVertical.Component(value: 45, color: NSColor.magentaColor()),
+            Chart.BarsVertical.Component(value: 55, color: NSColor.redColor()),
+            Chart.BarsVertical.Component(value: 66, color: NSColor.blueColor())
         ]
         let barsVertical = Chart.BarsVertical(components: barComponents)
+        let renderer = Chart.Renderer(data: barsVertical, fontSize: 100)
+        let tiffImage = renderer.TIFFImage
+        let imageView = NSImageView()
+        imageView.image = tiffImage
         
-        let ringsComponents = [
-            Chart.Rings.Component(size: 120, color: NSColor.blueColor()),
-            Chart.Rings.Component(size: 100, color: NSColor.redColor()),
-            Chart.Rings.Component(size: 20, color: NSColor.yellowColor()),
-            Chart.Rings.Component(size: 32, color: NSColor.greenColor()),
-            Chart.Rings.Component(size: 75, color: NSColor.blackColor()),
-            Chart.Rings.Component(size: 89, color: NSColor.grayColor()),
-            Chart.Rings.Component(size: 45, color: NSColor.darkGrayColor()),
-            Chart.Rings.Component(size: 22, color: NSColor.lightGrayColor()),
-            Chart.Rings.Component(size: 12, color: NSColor.cyanColor()),
-            Chart.Rings.Component(size: 45, color: NSColor.magentaColor()),
-            Chart.Rings.Component(size: 55, color: NSColor.redColor()),
-            Chart.Rings.Component(size: 66, color: NSColor.blueColor())
-
-        ]
-        let rings = Chart.Rings(components: ringsComponents)
-        
-        let ringRenderer = Chart.Rings.Renderer(data: rings, pointSize: 100)
-        let ringTIFF = ringRenderer.TIFFImage
-        let ringImageView = NSImageView()
-        ringImageView.image = ringTIFF
-        self.imageStackView?.insertView(ringImageView, atIndex: 0, inGravity: .Top)
-        
-        let pieComponents = [
-            Chart.Pie.Component(percentage: 5, color: NSColor.blueColor()),
-            Chart.Pie.Component(percentage: 10, color: NSColor.redColor()),
-            Chart.Pie.Component(percentage: 20, color: NSColor.yellowColor()),
-            Chart.Pie.Component(percentage: 5, color: NSColor.greenColor()),
-            Chart.Pie.Component(percentage: 11, color: NSColor.blackColor()),
-            Chart.Pie.Component(percentage: 2, color: NSColor.grayColor()),
-            Chart.Pie.Component(percentage: 2, color: NSColor.darkGrayColor()),
-            Chart.Pie.Component(percentage: 3, color: NSColor.lightGrayColor()),
-            Chart.Pie.Component(percentage: 15, color: NSColor.cyanColor()),
-            Chart.Pie.Component(percentage: 7, color: NSColor.magentaColor()),
-            Chart.Pie.Component(percentage: 8, color: NSColor.yellowColor()),
-            Chart.Pie.Component(percentage: 9, color: NSColor.orangeColor()),
-            Chart.Pie.Component(percentage: 8, color: NSColor.yellowColor()),
-            Chart.Pie.Component(percentage: 9, color: NSColor.orangeColor())
-        ]
-        let pie = Chart.Pie(components: pieComponents)
-
-        
-        let pieRenderer = Chart.Pie.Renderer(data: pie, pointSize: 100)
-        let pieTIFF = pieRenderer.TIFFImage
-        let pieImageView = NSImageView()
-        pieImageView.image = pieTIFF
-        self.imageStackView?.insertView(pieImageView, atIndex: 0, inGravity: .Top)
-
+        self.imageStackView?.addView(imageView, inGravity: .Top)
     }
 }
