ba044e80f1560cc6f5d77c631c08de8acbe2f194
Jeffrey Bergier <jeffburg@jeffburg.com>
2015-11-24 20:17:01 -0800

    added some extra code to the singleton so that it has a unique label for each type of chart. Also, it releases the labels after a second.

--- SSChartwell/SSChartwell_iOS/iOSChartProtocolExtensions.swift
+++ SSChartwell/SSChartwell_iOS/iOSChartProtocolExtensions.swift
@@ -14,7 +14,7 @@ public extension ChartRendererType {
         let font = CTFont.chartwellFont(self.data, pointSize: self.fontSize)
         let attributedString = NSAttributedString(chartData: self.data, font: font)
         
-        let renderingView = LabelSingleton.sharedLabel.label
+        let renderingView = LabelSingleton.sharedLabel.labelForStyle(self.data?.dynamicType.style ?? Chart.Style.Bars)
         renderingView.attributedText = attributedString
         renderingView.sizeToFit()
         
@@ -63,11 +63,84 @@ public extension ChartRendererType {
 private class LabelSingleton {
     static let sharedLabel = LabelSingleton()
     
-    let label: UILabel = {
-        let renderingView = UILabel()
-        renderingView.backgroundColor = UIColor.whiteColor()
-        renderingView.lineBreakMode = NSLineBreakMode.ByClipping
-        renderingView.sizeToFit()
-        return renderingView
-    }()
+    private var barsLabel: UILabel?
+    private var barsVerticalLabel: UILabel?
+    private var linesLabel: UILabel?
+    private var piesLabel: UILabel?
+    private var radarLabel: UILabel?
+    private var ringsLabel: UILabel?
+    private var roseLabel: UILabel?
+    
+    private var resetTimer: NSTimer?
+
+    @objc private func resetTimerFired(timer: NSTimer?) {
+        timer?.invalidate()
+        self.resetTimer?.invalidate()
+        self.resetTimer = .None
+        
+        self.resetTimer = .None
+        self.barsLabel = .None
+        self.barsVerticalLabel = .None
+        self.linesLabel = .None
+        self.piesLabel = .None
+        self.radarLabel = .None
+        self.ringsLabel = .None
+        self.roseLabel = .None
+    }
+    
+    func labelForStyle(style: Chart.Style) -> UILabel {
+        var existingLabel: UILabel?
+        
+        switch style {
+        case .Bars:
+            existingLabel = self.barsLabel
+        case .BarsVertical:
+            existingLabel = self.barsVerticalLabel
+        case .Lines:
+            existingLabel = self.linesLabel
+        case .Pies:
+            existingLabel = self.piesLabel
+        case .Radar:
+            existingLabel = self.radarLabel
+        case .Rings:
+            existingLabel = self.ringsLabel
+        case .Rose:
+            existingLabel = self.roseLabel
+        }
+        
+        if existingLabel == .None {
+            if self.resetTimer == .None {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.resetTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "resetTimerFired:", userInfo: .None, repeats: false)
+                }
+            }
+            
+            existingLabel = {
+                let label = UILabel()
+                label.backgroundColor = UIColor.whiteColor()
+                label.lineBreakMode = NSLineBreakMode.ByClipping
+                label.sizeToFit()
+                return label
+            }()
+            
+            switch style {
+            case .Bars:
+                self.barsLabel = existingLabel
+            case .BarsVertical:
+                self.barsVerticalLabel  = existingLabel
+            case .Lines:
+                self.linesLabel = existingLabel
+            case .Pies:
+                self.piesLabel = existingLabel
+            case .Radar:
+                self.radarLabel = existingLabel
+            case .Rings:
+                self.ringsLabel = existingLabel
+            case .Rose:
+                self.roseLabel = existingLabel
+            }
+        }
+        
+        return existingLabel!
+    }
 }
